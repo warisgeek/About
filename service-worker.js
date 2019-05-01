11c374886ea316de33ecd5c547e5e1c8394fb874
waris@iprocessdata.com <waris@iprocessdata.com>
2019-05-01 23:01:41 +0530

    First Commit

---
+++ service-worker.js
@@ -0,0 +1,93 @@
+// limitations under the License.
+
+var dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA-final-1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/animate.css',
+    'css/icomoon.css',
+    'css/bootstrap.css',
+    'css/font-awesome.min.css',
+    'css/style.css',
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700',
+    'https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700',
+    '/images/about.jpg',
+    'js/jquery.min.js',
+    'js/jquery.easing.1.3.js',
+    'js/bootstrap.min.js',
+    'js/jquery.waypoints.min.js',
+    'js/jquery.countTo.js',
+    'js/main.js'
+
+
+
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    /*
+     * Fixes a corner case in which the app wasn't returning the latest data.
+     * You can reproduce the corner case by commenting out the line below and
+     * then doing the following steps: 1) load app for first time so that the
+     * initial New York City data is shown 2) press the refresh button on the
+     * app 3) go offline 4) reload the app. You expect to see the newer NYC
+     * data, but you actually see the initial data. This happens because the
+     * service worker is not yet activated. The code below essentially lets
+     * you activate the service worker faster.
+     */
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+    if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * weather data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+        e.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response) {
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+});
