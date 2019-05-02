70405975b753b2f8bba9c611366bfb681c15a35f
Waris Ali <warisaliniit@gmail.com>
2019-05-02 19:56:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 var dataCacheName = 'weatherData-v1';
 var cacheName = 'weatherPWA-final-1';
 var filesToCache = [
-    '/',
+    'index.html',
     '404.html',
     'css/animate.css',
     'css/icomoon.css',
